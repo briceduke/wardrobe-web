de35e0107b2309eb5ee8b90a5599a9df07893eb8
Brice Duke <bricewduke@gmail.com>
2022-04-11 18:07:53 -0500

    chore(global): add tailwind and daisyui

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,5 @@
+import '../styles/globals.css';
+
 import { AppProps } from 'next/app';
 import { Provider } from 'react-redux';
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import { NextPage } from 'next';
 
 const HomePage: NextPage = () => {
-	return <div>HomePage</div>;
+	return <div className="text-2xl">HomePage</div>;
 };
 
 export default HomePage;
