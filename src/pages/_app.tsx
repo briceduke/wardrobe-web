473a083016e433d15b543746772db4e97b4fa34f
Brice Duke <bricewduke@gmail.com>
2022-04-11 18:04:21 -0500

    chore(global): set up next and rtk

---
+++ src/pages/_app.tsx
@@ -0,0 +1,14 @@
+import { AppProps } from 'next/app';
+import { Provider } from 'react-redux';
+
+import { store } from '../app/store';
+
+const App = ({ Component, pageProps }: AppProps) => {
+	return (
+		<Provider store={store}>
+			<Component {...pageProps} />
+		</Provider>
+	);
+};
+
+export default App;
